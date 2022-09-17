ff787bb82b004b74b1b4637dcd68031a00f084d3
oliverrai <raiane.mateus.oliveira@gmail.com>
2022-09-17 20:36:44 -0300

    adicionando arquivos staticos

--- receitas/views.py
+++ receitas/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Receitas</h1>')
+    return render(request, 'index.html')
+
+def receita(request):
+    return render(request, 'receita.html')
