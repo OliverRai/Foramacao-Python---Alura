9ea4d69c357fc1103caa6d4c2d2a7bbe5f2dc125
oliverrai <raiane.mateus.oliveira@gmail.com>
2022-09-17 21:44:55 -0300

    adicionando bd

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Receita
 
-# Register your models here.
+admin.site.register(Receita)
+
+
+# criar usuario admin
+# python manage.py createsuperuser
